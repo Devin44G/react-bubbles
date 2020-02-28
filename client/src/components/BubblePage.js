23d4704324d25f7a53e4a8313ce7c8dd692e899c
Devin44G <35251347+Devin44G@users.noreply.github.com>
2020-02-28 11:08:09 -0600

    Made an axiosWithAuth, which takes in a token and ste it to localStorage -> Made PrivatedRoute for BubblePage.js which is pointed to the path '/protected' -> Bubbles and color render to the page when logged in . . .

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
@@ -7,6 +8,15 @@ import ColorList from "./ColorList";
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
+  useEffect(() => {
+    axiosWithAuth()
+      .get('/api/colors')
+      .then(res => {
+        console.log('DATA: ', res.data);
+        setColorList(res.data);
+      })
+      .catch(err => console.error('THERE HAS BEEN AN ERROR: ', err));
+  }, []);
   // set that data to the colorList state property
 
   return (
